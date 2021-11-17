0189dfc19b79d4cd42638bfbd414349e9a157550
ironartisan <54694467+ironartisan@users.noreply.github.com>
2021-11-17 11:38:42 +0800

    添加 54.螺旋矩阵题解

---
+++ python/54.螺旋矩阵.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author : cyl
+# @Time : 2021/11/17 10:36
+from typing import List
+
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        if not matrix or not matrix[0]:
+            return list()
+
+        rows, columns = len(matrix), len(matrix[0])
+        order = list()
+        left, right, top, bottom = 0, columns - 1, 0, rows - 1
+        while left <= right and top <= bottom:
+            for column in range(left, right + 1):  # 从左到右
+                order.append(matrix[top][column])
+            for row in range(top + 1, bottom + 1):  # 从上到下
+                order.append(matrix[row][right])
+            if left < right and top < bottom:
+                for column in range(right - 1, left, -1):  # 从右到左
+                    order.append(matrix[bottom][column])
+                for row in range(bottom, top, -1):  # 从下到上
+                    order.append(matrix[row][left])
+            left, right, top, bottom = left + 1, right - 1, top + 1, bottom - 1
+        return order
