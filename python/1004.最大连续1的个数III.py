cc3d2fc088efc969659b474b90099b2c8502ffee
ironartisan <54694467+ironartisan@users.noreply.github.com>
2021-11-25 16:56:35 +0800

    Add 1004&&220 md

---
+++ python/1004.最大连续1的个数III.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author : cyl
+# @Time : 2021/11/25 16:46 
+from typing import List
+
+
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        pass
---
+++ python/220.存在重复元素III.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author : cyl
+# @Time : 2021/11/25 16:48 
+from typing import List
+
+
+class Solution:
+    def containsNearbyAlmostDuplicate(self, nums: List[int], k: int, t: int) -> bool:
+        pass
