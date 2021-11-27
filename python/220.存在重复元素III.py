0cfd1b1b39c37582f7792df4eecec7467a4b4c1a
ironartisan <54694467+ironartisan@users.noreply.github.com>
2021-11-27 18:45:04 +0800

    Update 220 Solution

--- python/220.存在重复元素III.py
+++ python/220.存在重复元素III.py
@@ -3,8 +3,47 @@
 # @Author : cyl
 # @Time : 2021/11/25 16:48 
 from typing import List
-
+from sortedcontainers import SortedList
+import bisect
 
 class Solution:
     def containsNearbyAlmostDuplicate(self, nums: List[int], k: int, t: int) -> bool:
-        pass
+        # O(N logk)
+        window = SortedList()
+        for i in range(len(nums)):
+            # len(window) == k
+            if i > k:
+                window.remove(nums[i - 1 - k])
+            window.add(nums[i])
+            idx = bisect.bisect_left(window, nums[i])
+            if idx > 0 and abs(window[idx] - window[idx-1]) <= t:
+                return True
+            if idx < len(window) - 1 and abs(window[idx+1] - window[idx]) <= t:
+                return True
+        return False
+
+
+    def containsNearbyAlmostDuplicate2(self, nums: List[int], k: int, t: int) -> bool:
+        bucket_dict = dict()
+        for i in range(len(nums)):
+            # 将 nums[i] 划分到大小为 t + 1 的不同桶中
+            num = nums[i] // (t + 1)
+
+            # 桶中已经有元素了
+            if num in bucket_dict:
+                return True
+
+            # 把 nums[i] 放入桶中
+            bucket_dict[num] = nums[i]
+
+            # 判断左侧桶是否满足条件
+            if (num - 1) in bucket_dict and abs(bucket_dict[num - 1] - nums[i]) <= t:
+                return True
+            # 判断右侧桶是否满足条件
+            if (num + 1) in bucket_dict and abs(bucket_dict[num + 1] - nums[i]) <= t:
+                return True
+            # 将 i-k 之前的旧桶清除，因为之前的桶已经不满足条件了
+            if i >= k:
+                bucket_dict.pop(nums[i-k] // (t + 1))
+
+        return False
